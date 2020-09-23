e498fd031854eaaa3abb1a4f76556f01f16145f0
Agustin <avadino@itba.edu.ar>
2020-09-23 17:26:37 -0300

    Ultimo fix de main.c

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 extern int read(int fd,char * buff,unsigned int bytes);
 extern int write(int fd, char * buff,unsigned int bytes);
 
-#define SIZE 16
+#define SIZE 17
 
 void swapBuffer(char* buffer,int bytes){
 	int i;
 	char aux;
-	for(i=0;i<=bytes/2;i++){
+	for(i=0;i<bytes/2;i++){
 		aux=buffer[i];
 		buffer[i]=buffer[bytes-i-1];
 		buffer[bytes-i-1]=aux;
@@ -20,10 +20,10 @@ int main(int argc, char * argv[]){
 	int bytes;
 	bytes=read(0,buffer,SIZE);
 	if(bytes<=16){
-	swapBuffer(buffer,bytes-1);
-	write(1,buffer,bytes);
+		swapBuffer(buffer,bytes-1);
+		write(1,buffer,bytes);
 	}else{
-	write(1,"Se ingreso mas de 15 caracteres",32);
+		write(1,"Se ingreso mas de 15 caracteres.\n",34);
 	}
 	return 0;
 }
